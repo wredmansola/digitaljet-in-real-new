3bac417fc295bcf891e5b3a1b3e771108781de9d
Wredman Sola <65522339+wredmansola@users.noreply.github.com>
2021-03-07 02:18:29 -0500

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
+    title: 'DigitalJet - digital learning hub',
     description:
-      'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+      'Digitaljet is blog where you get all digital learning. From Android, Windows to all your favourite one. Sharing daily digital trending offers from where you can buy at cheap than any one else.',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
